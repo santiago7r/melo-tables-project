edc81e624ff84d8147dbb243eb6ae352ece49dc3
Eddy Rhenals <eddy.rhenals7@gmail.com>
2024-09-30 20:13:54 -0500

    Create button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface Props {
+    className: string,
+    onClick: () => void,
+    label: string
+}
+
+export default function Button({ className, onClick, label}: Props) {
+  return <button className={className} onClick={onClick}>{label}</button>
+}
